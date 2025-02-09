3b59b15f0de501d10bd692fc15819224d0c800ac
shinnkura <n.shinzo1010@icloud.com>
2025-02-09 15:49:07 +0900

    カテゴリーを投稿できるように修正

--- internal/wp/category.go
+++ internal/wp/category.go
@@ -1,6 +1,9 @@
 package wp
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
@@ -26,13 +29,61 @@ func GetCategoryIDs(client *Client, categoryNames []string) ([]int, error) {
 
 	var categoryIDs []int
 	for _, name := range categoryNames {
+		found := false
 		for _, cat := range categories {
 			if cat.Name == name {
 				categoryIDs = append(categoryIDs, cat.ID)
+				found = true
 				break
 			}
+		}
+
+		if !found {
+			// カテゴリーが存在しない場合は新規作成
+			newCat, err := CreateCategory(client, name)
+			if err != nil {
+				return nil, fmt.Errorf("カテゴリー作成エラー: %v", err)
+			}
+			categoryIDs = append(categoryIDs, newCat.ID)
 		}
 	}
 
 	return categoryIDs, nil
+}
+
+type CreateCategoryRequest struct {
+	Name string `json:"name"`
+}
+
+func CreateCategory(client *Client, name string) (*Category, error) {
+	categoryReq := CreateCategoryRequest{
+		Name: name,
+	}
+
+	jsonData, err := json.Marshal(categoryReq)
+	if err != nil {
+		return nil, fmt.Errorf("JSON変換エラー: %v", err)
+	}
+
+	url := client.BaseURL + "/wp-json/wp/v2/categories"
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonData))
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Authorization", "Basic "+client.BasicAuth)
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := client.HTTPClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var category Category
+	if err := client.decodeResponse(resp, &category); err != nil {
+		return nil, err
+	}
+
+	return &category, nil
 }
